19204d2e87df5c4eba0f16871190313aec152537
Jonathan Pohl <jonathan@pohls.email>
2020-02-03 15:34:47 +0100

    DatenSchnittstelle: laden implementiert

--- sepp-main/src/test/java/sepp/daten/DatenSchnittstelleImplTest.java
+++ sepp-main/src/test/java/sepp/daten/DatenSchnittstelleImplTest.java
@@ -8,8 +8,16 @@ public class DatenSchnittstelleImplTest {
 
 	// 001
 	@Test(expected = Test.None.class)
-	public void testGetZeilenArray() throws IOException {
+	public void testGetDateiInformationen() throws IOException {
 		DatenSchnittstelle schnittstelle = new DatenSchnittstelleImpl();
 		schnittstelle.getDateiInformationen("C:\\Java\\0-sepp\\Dokument.rtf");
+	}
+
+	// 002
+	@Test(expected = Test.None.class)
+	public void testKommentar() throws IOException {
+		DatenSchnittstelle schnittstelle = new DatenSchnittstelleImpl();
+		DateiInformationen data = schnittstelle.getDateiInformationen("C:\\Java\\0-sepp\\Dokument.rtf");
+		System.out.println(data.getToDos());
 	}
 }
