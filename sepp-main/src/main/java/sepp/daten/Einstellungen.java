f323da7ed36758b9661f1139edd6216f7bd4651e
Jonathan Pohl <jonathan@pohls.email>
2020-02-03 12:33:05 +0100

    Einstellungen laden + test, DateiHelfer hinzugefügt

--- sepp-main/src/main/java/sepp/daten/Einstellungen.java
+++ sepp-main/src/main/java/sepp/daten/Einstellungen.java
@@ -1,13 +1,16 @@
 package sepp.daten;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
 public class Einstellungen {
 
 	private final String DATEINAME = "/sepp_config.properties";
+	String pfad = System.getProperty("user.home") + DATEINAME;
 
 	String projektPfad;
 	String username;
@@ -20,21 +23,21 @@ public class Einstellungen {
 		return username;
 	}
 
-	public void speichern() {
-		String pfad = System.getProperty("user.home");
-		try (OutputStream output = new FileOutputStream(pfad + DATEINAME)) {
+	public void speichern() throws IOException {
+		OutputStream output = new FileOutputStream(pfad);
+		Properties einstellungen = new Properties();
+		einstellungen.setProperty("benutzer", username);
+		einstellungen.setProperty("projekt", projektPfad);
+		einstellungen.store(output, null);
 
-			Properties einstellungen = new Properties();
-			einstellungen.setProperty("benutzer", username);
-			einstellungen.setProperty("projekt", projektPfad);
-
-			// save properties to project root folder
-			einstellungen.store(output, null);
-
-			System.out.println(einstellungen);
-
-		} catch (IOException io) {
-			io.printStackTrace();
-		}
 	}
+
+	public void laden() throws IOException {
+		InputStream input = new FileInputStream(pfad);
+		Properties einstellungen = new Properties();
+		einstellungen.load(input);
+		username = einstellungen.getProperty("benutzer");
+		projektPfad = einstellungen.getProperty("projekt");
+	}
+
 }
--- sepp-main/src/test/java/sepp/daten/EinstellungenTest.java
+++ sepp-main/src/test/java/sepp/daten/EinstellungenTest.java
@@ -2,19 +2,75 @@ package sepp.daten;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class EinstellungenTest {
 
-	@Test
-	public void testUsernameNull() {
+	// 001
+	@Test(expected = Test.None.class)
+	public void testUsernameNull() throws IOException {
 		Einstellungen test = new Einstellungen();
 		assertEquals(null, test.getUsername());
 	}
 
-	public void testUsernameSet() {
+	// 002
+	@Test(expected = Test.None.class)
+	public void testUsernameSet() throws IOException {
 		Einstellungen test = new Einstellungen();
 		test.username = "testName";
 		assertEquals("testName", test.getUsername());
+	}
+
+	// 003
+	@Test(expected = Test.None.class)
+	public void testProjektPfadNull() throws IOException {
+		Einstellungen test = new Einstellungen();
+		assertEquals(null, test.getProjektPfad());
+	}
+
+	// 004
+	@Test(expected = Test.None.class)
+	public void testProjektPfadSet() throws IOException {
+		Einstellungen test = new Einstellungen();
+		test.projektPfad = "testPfad";
+		assertEquals("testPfad", test.getProjektPfad());
+	}
+
+	// 005
+	@Test(expected = Test.None.class)
+	public void testSpeichern() throws IOException {
+		Einstellungen test = new Einstellungen();
+		test.projektPfad = "testPfad";
+		test.username = "testName";
+		test.speichern();
+	}
+
+	// 006
+	@Test(expected = Test.None.class)
+	public void testLaden() throws IOException {
+		Einstellungen test = new Einstellungen();
+		test.projektPfad = "eins";
+		test.username = "zwei";
+		test.speichern();
+		test.laden();
+		assertEquals("eins", test.getProjektPfad());
+		assertEquals("zwei", test.getUsername());
+	}
+
+	// 007
+	@Test(expected = Test.None.class)
+	public void testLadenError() throws IOException {
+		String pfad = System.getProperty("user.home") + "\\sepp_config.properties";
+		File f = new File(pfad);
+		if (f.delete()) {
+			System.out.println(f.getName() + " deleted");
+		} else {
+			System.out.println("failed");
+		}
+		Einstellungen test = new Einstellungen();
+		test.laden();
 	}
 }
