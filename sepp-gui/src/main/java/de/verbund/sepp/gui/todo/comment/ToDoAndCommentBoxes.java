d69b888890d41517fea6b5e3a9aa3a420d7a749d
Jonathan Pohl <jonathan@pohls.email>
2020-02-04 13:18:17 +0100

    ToDoAndCommentBoxes: Pfad relativ zum Projektpfad

--- sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/ToDoAndCommentBoxes.java
+++ sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/ToDoAndCommentBoxes.java
@@ -8,6 +8,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 import de.verbund.sepp.main.daten.DateiInformationen;
+import de.verbund.sepp.main.daten.DatenSchnittstelle;
 import de.verbund.sepp.main.daten.DatenSchnittstelleImpl;
 import de.verbund.sepp.main.utils.DateiInfoHelfer;
 
@@ -26,36 +27,40 @@ public class ToDoAndCommentBoxes {
 	private JTable createCommentTable() {
 		dataSchnittstelle = new DatenSchnittstelleImpl();
 		DateiInformationen dataComments;
-		
+
 		try {
-			dataComments = dataSchnittstelle.getDateiInformationen("C:\\Users\\Administrator\\git\\sepp\\TestOrdner\\projekt.sepp");
+			dataComments = dataSchnittstelle.getDateiInformationen(dataSchnittstelle.getEinstellungen().getProjektPfad()
+					+ "\\" + DatenSchnittstelle.PRIMAER_DATEINAME);
 			String[][] userAndComments = DateiInfoHelfer.getZeilenArray(dataComments.getKommentare());
 			String[] columns = { "User", "Kommentar" };
-			TableModel model = new DefaultTableModel(userAndComments, columns);
-			JTable doneTable = new TableAndPopUpMenu(model).getTable();
-			return doneTable;
+			return gefuellteTabelle(userAndComments, columns);
 		} catch (IOException e) {
 			System.out.println("Fehler Comments");
 			return null;
 		}
-		
+
 	}
 
 	private JTable createToDoTable() {
 		dataSchnittstelle = new DatenSchnittstelleImpl();
 		DateiInformationen dataToDos;
-		
+
 		try {
-			dataToDos = dataSchnittstelle.getDateiInformationen("C:\\Users\\Administrator\\git\\sepp\\TestOrdner\\projekt.sepp");
+			dataToDos = dataSchnittstelle.getDateiInformationen(dataSchnittstelle.getEinstellungen().getProjektPfad()
+					+ "\\" + DatenSchnittstelle.PRIMAER_DATEINAME);
 			String[][] userAndToDos = DateiInfoHelfer.getZeilenArray(dataToDos.getToDos());
 			String[] columns = { "User", "To Do's" };
-			TableModel model = new DefaultTableModel(userAndToDos, columns);
-			JTable doneTable = new TableAndPopUpMenu(model).getTable();
-			return doneTable;
+			return gefuellteTabelle(userAndToDos, columns);
 		} catch (IOException e) {
 			System.out.println("Fehler ToDo's");
 			return null;
 		}
+	}
+
+	private JTable gefuellteTabelle(String[][] inhalte, String[] spaltenTitel) {
+		TableModel model = new DefaultTableModel(inhalte, spaltenTitel);
+		JTable doneTable = new TableAndPopUpMenu(model).getTable();
+		return doneTable;
 	}
 
 	public JScrollPane getCommentBox() {
