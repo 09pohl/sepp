96b73e9dce53a70922d8c98d27f48ec2c86ae71e
Daniel Schmidt <dniosc@gmail.com>
2020-02-04 15:40:58 +0100

    mit enums

--- sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/TableAndPopUpMenu.java
+++ sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/TableAndPopUpMenu.java
@@ -13,80 +13,90 @@ import javax.swing.table.TableModel;
 public class TableAndPopUpMenu {
 
 	private JTable table;
-	
+
 	public TableAndPopUpMenu(TableModel model) {
 		table = new JTable(model);
 	}
-	
-	private JTable createTable(){
-		
-		table.addMouseListener(new MouseAdapter() {
-		    @Override
-		    public void mouseReleased(MouseEvent e) {
-		        int r = table.rowAtPoint(e.getPoint());
-		        if (r >= 0 && r < table.getRowCount()) {
-		        	table.setRowSelectionInterval(r, r);
-		        } else {
-		        	table.clearSelection();
-		        }
 
-		        int rowindex = table.getSelectedRow();
-		        if (rowindex < 0)
-		            return;
-		        if (e.isPopupTrigger() && e.getComponent() instanceof JTable ) {
-		            JPopupMenu popup = createPopUpMenu(new JPopupMenu());
-		            popup.show(e.getComponent(), e.getX(), e.getY());
-		        }
-		    }
+	private JTable createTable() {
+
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				int r = table.rowAtPoint(e.getPoint());
+				if (r >= 0 && r < table.getRowCount()) {
+					table.setRowSelectionInterval(r, r);
+				} else {
+					table.clearSelection();
+				}
+
+				int rowindex = table.getSelectedRow();
+				if (rowindex < 0)
+					return;
+				if (e.isPopupTrigger() && e.getComponent() instanceof JTable) {
+					JPopupMenu popup = createPopUpMenu(new JPopupMenu());
+					popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+			}
 
 			private JPopupMenu createPopUpMenu(JPopupMenu popup) {
-				popup.add("Hinzufügen").addActionListener(e -> addOrEditJOP("add"));
-				popup.add("Editieren").addActionListener(e -> addOrEditJOP("edit"));
+				popup.add("Hinzufügen").addActionListener(e -> addOrEditJOP(AddOrEdit.ADD));
+				popup.add("Editieren").addActionListener(e -> addOrEditJOP(AddOrEdit.EDIT));
 				popup.add("Löschen").addActionListener(e -> deleteJOP());
 				return popup;
 			}
 		});
 		table.setEnabled(false);
 		return table;
-		}
+	}
 
 	public JTable getTable() {
 		return createTable();
-		
+
 	}
-	private void addOrEditJOP(String text) {
-		String addOrEdit;
-		
-		if(text.equals("add")){
-			addOrEdit = "Hinzufügen";
-		}
-		else {
-			addOrEdit = "Bearbeiten";
-		}
+
+	private void addOrEditJOP(AddOrEdit text) {
 		
 		String comment = "";
-		boolean content = false;
-		boolean cancel = false;
-		
-		while (content == false || cancel == false) {
-			comment = JOptionPane.showInputDialog(null, addOrEdit + ":", addOrEdit, JOptionPane.OK_CANCEL_OPTION);
+		boolean goOn = true;
+
+		while (goOn) {
+			comment = JOptionPane.showInputDialog(null, text.getInfoText() + ":", text.getInfoText(), JOptionPane.OK_CANCEL_OPTION);
 			try {
 				if (!comment.replaceAll("\\s+", "").isEmpty()) {
 					System.out.println(comment);
-					content = true;
+					goOn = false;
+					//-->Funktionen
 				}
-			}catch (NullPointerException npe) {
-				cancel = true;
+			} catch (NullPointerException npe) {
+				goOn = false;
 			}
 		}
 	}
 	
-	private void deleteJOP() {
-		int doDelete = JOptionPane.showConfirmDialog(null, "Wollen Sie diese Zeile wirklich löschen?", "Löschen", JOptionPane.YES_NO_OPTION);
+	enum AddOrEdit {
+		ADD("Hinzufügen"), EDIT("Bearbeiten");
+		private String infoText;
 		
-		if (doDelete==JOptionPane.YES_OPTION) {
+		AddOrEdit(String infoText){
+			this.infoText = infoText;
+		}
+		
+		private String getInfoText() {
+			return infoText;
+		}
+
+
+	}
+
+	private void deleteJOP() {
+		int doDelete = JOptionPane.showConfirmDialog(null, "Wollen Sie diese Zeile wirklich löschen?", "Löschen",
+				JOptionPane.YES_NO_OPTION);
+
+		if (doDelete == JOptionPane.YES_OPTION) {
 			System.out.println("DoDelete");
+			//Funktion
 		}
-		}
+	}
 
 }
