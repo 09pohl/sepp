9bd58495e1a4f173692141ee88f23ad25cabf442
Friedrich Kiltz <f@kiltz.de>
2020-02-04 09:38:28 +0100

    Kleine Aktualisierungen

--- sepp-gui/src/main/java/todo/comment/ToDoAndCommentBoxes.java
+++ sepp-gui/src/main/java/todo/comment/ToDoAndCommentBoxes.java
@@ -1,61 +1,54 @@
 package todo.comment;
 
-import java.awt.Dimension;
-
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 import sepp.daten.DateiInformationen;
-import sepp.utils.DateiHelfer;
 import sepp.utils.DateiInfoHelfer;
 
-public class ToDoAndCommentBoxes extends JPanel{
-	
-	private JTable doneTable;
+public class ToDoAndCommentBoxes {
+
 	private DateiInformationen dataComments;
 	private DateiInformationen dataToDos;
-	private JScrollPane scrollPaneComments;
-	private JScrollPane scrollPaneToDos;
-	
+
 	private JScrollPane initCommentBox() {
-		setSize(500, 300);
-		scrollPaneComments = new JScrollPane(createCommentTable());
-		return scrollPaneComments;
-		}
-	
+
+		return new JScrollPane(createCommentTable());
+	}
+
 	private JScrollPane initToDoBox() {
-		setSize(500, 300);
-		scrollPaneComments = new JScrollPane(createToDoTable());
-		return scrollPaneToDos;
-		}
-			
+
+		return new JScrollPane(createToDoTable());
+	}
+
 	private JTable createCommentTable() {
 		dataComments = new DateiInformationen();
 		dataComments.setKommentare("Daniel:Schmidt\nLukas:Lüthke\nJonathan:Pohl\nDuong:Le\nDuong:Le");
 		String[][] userAndComments = DateiInfoHelfer.getZeilenArray(dataComments.getKommentare());
-		String[] columns = {"User", "Kommentar"};
+		String[] columns = { "User", "Kommentar" };
 		TableModel model = new DefaultTableModel(userAndComments, columns);
-		doneTable = new JTable(model);
+		JTable doneTable = new JTable(model);
 		return doneTable;
 	}
-	
+
 	private JTable createToDoTable() {
 		dataToDos = new DateiInformationen();
-		dataToDos.setToDos("Daniel:Schmidt\nLukas:Lüthke\nJonathan:Pohl\nDuong:Le\nDuong:Le\nDaniel:Schmidt\nLukas:Lüthke\nJonathan:Pohl\nDuong:Le\nDuong:Le");
+		dataToDos.setToDos(
+				"Daniel:Schmidt\nLukas:Lüthke\nJonathan:Pohl\nDuong:Le\nDuong:Le\nDaniel:Schmidt\nLukas:Lüthke\nJonathan:Pohl\nDuong:Le\nDuong:Le");
 		String[][] userAndToDos = DateiInfoHelfer.getZeilenArray(dataToDos.getToDos());
-		String[] columns = {"User", "To Do's"};
+		String[] columns = { "User", "To Do's" };
 		TableModel model = new DefaultTableModel(userAndToDos, columns);
-		doneTable = new JTable(model);
+		JTable doneTable = new JTable(model);
 		return doneTable;
 	}
-	
-	public JScrollPane getCommentBox(){
+
+	public JScrollPane getCommentBox() {
 		return initCommentBox();
 	}
-	public JScrollPane getToDoBox(){
+
+	public JScrollPane getToDoBox() {
 		return initToDoBox();
 	}
 }
