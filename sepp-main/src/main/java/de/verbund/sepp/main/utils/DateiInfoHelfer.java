25a9110af5de56113b987799b8aa083981d32502
Jonathan Pohl <jonathan@pohls.email>
2020-02-05 13:30:21 +0100

    DateiInfoHelfer: dateiInformationenListe zur einfacheren Verarbeitung der Projekt-Dateien

--- sepp-main/src/main/java/de/verbund/sepp/main/utils/DateiInfoHelfer.java
+++ sepp-main/src/main/java/de/verbund/sepp/main/utils/DateiInfoHelfer.java
@@ -1,5 +1,12 @@
 package de.verbund.sepp.main.utils;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import de.verbund.sepp.main.daten.DateiInformationen;
+import de.verbund.sepp.main.daten.DatenSchnittstelle;
+import de.verbund.sepp.main.daten.DatenSchnittstelleImpl;
+
 public class DateiInfoHelfer {
 
 	public static String[][] getZeilenArray(String text) {
@@ -10,6 +17,16 @@ public class DateiInfoHelfer {
 			for (int i = 0; i < zeilen.length; i++) {
 				result[i] = zeilen[i].split(":", 2);
 			}
+		}
+		return result;
+	}
+
+	public static ArrayList<DateiInformationen> dateiInformationenListe(ArrayList<String> dateiPfade)
+			throws IOException {
+		DatenSchnittstelle schnittstelle = new DatenSchnittstelleImpl();
+		ArrayList<DateiInformationen> result = new ArrayList<DateiInformationen>();
+		for (String pfad : dateiPfade) {
+			result.add(schnittstelle.getDateiInformationen(pfad));
 		}
 		return result;
 	}
