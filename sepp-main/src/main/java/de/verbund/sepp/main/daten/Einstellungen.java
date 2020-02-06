40ea465265ad7a45e960c1426c59cb07788fffb7
Jonathan Pohl <jonathan@pohls.email>
2020-02-06 12:23:45 +0100

    Einstellungen: properties in SEPP Verzeichnis

--- sepp-main/src/main/java/de/verbund/sepp/main/daten/Einstellungen.java
+++ sepp-main/src/main/java/de/verbund/sepp/main/daten/Einstellungen.java
@@ -1,5 +1,6 @@
 package de.verbund.sepp.main.daten;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -11,12 +12,14 @@ import de.verbund.sepp.main.utils.DateiHelfer;
 
 public class Einstellungen {
 
+	private final String EINSTELLUNGEN_ORDNER = "/SEPP";
 	private final String EINSTELLUNGEN_DATEINAME = "/sepp_config.properties";
 	private final String BENUTZER_PROPERTY = "benutzer";
 	private final String PROJEKT_PROPERTY = "projekt";
 
 	private static Einstellungen instance;
-	String einstellungenPfad = System.getProperty("user.home") + EINSTELLUNGEN_DATEINAME;
+	String einstellungenPfad = System.getProperty("user.home") + EINSTELLUNGEN_ORDNER + EINSTELLUNGEN_DATEINAME;
+	String einstellungenOrdnerPfad = System.getProperty("user.home") + EINSTELLUNGEN_ORDNER;
 
 	String projektPfad;
 	String username; // keine Doppelpunkte zulassen!
@@ -36,10 +39,15 @@ public class Einstellungen {
 	}
 
 	public void speichern() throws IOException {
+		File ordner = new File(einstellungenOrdnerPfad);
+		if (!ordner.exists()) {
+			ordner.mkdir();
+		}
 		OutputStream output = new FileOutputStream(einstellungenPfad);
 		Properties einstellungen = new Properties();
 		einstellungen.setProperty(BENUTZER_PROPERTY, username);
 		einstellungen.setProperty(PROJEKT_PROPERTY, projektPfad);
+
 		einstellungen.store(output, null);
 		DateiHelfer projektDatei = new DateiHelfer(projektPfad + "/" + DatenSchnittstelle.PRIMAER_DATEINAME);
 
