6c0909a3bf2eeb837c2609b016f0006990ef1f8d
Jonathan Pohl <jonathan@pohls.email>
2020-02-06 09:16:23 +0100

    Klassennamen

--- sepp-main/src/main/java/de/verbund/sepp/main/utils/StringHelfer.java
+++ sepp-main/src/main/java/de/verbund/sepp/main/utils/StringHelfer.java
@@ -2,16 +2,16 @@ package de.verbund.sepp.main.utils;
 
 public class StringHelfer {
 
-	public static boolean istNullLeerOderWhitespace(String text) {
+	public static boolean isNullEmptyOrWhitespace(String text) {
 		return text == null || text.trim().isEmpty();
 	}
 
-	public static boolean istNullOderLeer(String text) {
+	public static boolean isNullOrEmpty(String text) {
 		return text == null || text.isEmpty();
 	}
 
 	public static String ersterBuchstabeGross(String text) {
-		if (StringHelfer.istNullLeerOderWhitespace(text)) {
+		if (StringHelfer.isNullEmptyOrWhitespace(text)) {
 			return text;
 		}
 		return text.substring(0, 1).toUpperCase() + text.substring(1);
