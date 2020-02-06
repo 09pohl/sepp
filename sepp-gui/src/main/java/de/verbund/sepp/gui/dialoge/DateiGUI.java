032ce11883a150cc8791d2ce3467e65efad75f43
Lukas Lüthke <lukas.luethke@gmx.de>
2020-02-06 08:56:09 +0100

    wiedereinbinden der dateidarstellung in überarbeitete Dateigui

--- sepp-gui/src/main/java/de/verbund/sepp/gui/dialoge/DateiGUI.java
+++ sepp-gui/src/main/java/de/verbund/sepp/gui/dialoge/DateiGUI.java
@@ -2,51 +2,46 @@ package de.verbund.sepp.gui.dialoge;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.nio.file.attribute.FileTime;
 
 import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import de.verbund.sepp.main.daten.*;
 
-import java.nio.file.attribute.FileTime;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import de.verbund.sepp.main.daten.DateiInformationen;
+import de.verbund.sepp.main.daten.DatenSchnittstelle;
+import de.verbund.sepp.main.daten.DatenSchnittstelleImpl;
+import de.verbund.sepp.main.daten.Einstellungen;
+import de.verbund.sepp.main.utils.DatumHelfer;
 
-public class DateiGUI extends JPanel implements ActionListener{
-	
-	DatenSchnittstelle schnittstelle = DatenSchnittstelleImpl.getInstance();
-	Einstellungen einstellungen = schnittstelle.getEinstellungen();
+public class DateiGUI extends JPanel implements ActionListener {
+
+	Einstellungen einstellungen;
 	private JButton bInfo;
-	private static final DateTimeFormatter DATE_FORMATTER_WITH_TIME = DateTimeFormatter.ofPattern("MMM d, yyyy HH:mm:ss");
-	private String verzeichnis;
+	private String verzeichnis; // TODO - Dateiaufruf
 	private String name;
-	DateiInformationen data;
-	
+	DatenSchnittstelle schnittstelle = DatenSchnittstelleImpl.getInstance();
+	DateiInformationen data = schnittstelle.getDateiInformationen(verzeichnis);
+
 	public DateiGUI(String verzeichnis, String name) throws IOException {
-		this.name = name;
 		this.verzeichnis = verzeichnis;
-		data = schnittstelle.getDateiInformationen(verzeichnis);
-		//Container panel = getContentPane();
+		this.name = name;
 		setSize(350, 150);
-		//setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		//setLocationRelativeTo(null);
 		setLayout(new BorderLayout());
-		//setDefaultLookAndFeelDecorated(true);
-	    //setResizable(false);
-	    
-	    add(getNorden(), BorderLayout.NORTH);
-	    add(getMitte(), BorderLayout.CENTER);
-	    add(getSueden(), BorderLayout.SOUTH);
-	    setVisible(true); 
-	  }
 
+		add(getNorden(), BorderLayout.NORTH);
+		add(getMitte(), BorderLayout.CENTER);
+		add(getSueden(), BorderLayout.SOUTH);
+		setVisible(true);
+	}
 
-	//Laden von Icon und Namen
+	// Laden von Icon und Namen
 	private Component getNorden() {
 		Icon icon = data.getIcon();
 		JPanel p = new JPanel();
@@ -57,55 +52,34 @@ public class DateiGUI extends JPanel implements ActionListener{
 		return p;
 	}
 
-	//Laden von Datum
+	// Laden von Datum
 	private Component getMitte() throws IOException {
 		JPanel p = new JPanel();
 		FileTime datum_1 = data.getErstellungsDatum();
 		FileTime datum_2 = data.getAenderungsDatum();
-		String s = fileTimeToString(datum_1);
+		String s = DatumHelfer.fileTimeToString(datum_1);
 		JLabel label_1 = new JLabel("Erstellungsdatum: " + s);
-		String s1 = fileTimeToString(datum_2);
-		JLabel label_2 = new JLabel("Änderungsdatum: " + s1);
+		String s1 = DatumHelfer.fileTimeToString(datum_2);
+		JLabel label_2 = new JLabel("Aenderungsdatum: " + s1);
 		p.add(label_1);
 		p.add(label_2);
 		return p;
 	}
-	
-	//Laden von Button mit Verweis auf ToDo - und Kommentarliste
-	private Component getSueden(){
+
+	// Laden von Button mit Verweis auf ToDo - und Kommentarliste
+	private Component getSueden() {
 		JPanel p = new JPanel();
 		bInfo = new JButton("ToDo - & Kommentarliste");
 		bInfo.addActionListener(this);
 		p.add(bInfo);
 		return p;
 	}
-	
-	//DateTimeFormatter
-	public static String fileTimeToString(FileTime fileTime) {
-	      String s = parseToString(
-	              fileTime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
-	      return s;
-	}
 
-	public static FileTime fileTimeFromString(String dateTimeString) {
-	      LocalDateTime localDateTime = parseFromString(dateTimeString);
-	      return FileTime.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-	}
-	
-	public static String parseToString(LocalDateTime localDateTime) {
-	      return localDateTime.format(DATE_FORMATTER_WITH_TIME);
-	}
-
-	public static LocalDateTime parseFromString(String date) {
-	      return LocalDateTime.parse(date, DATE_FORMATTER_WITH_TIME);
-	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == bInfo) {
-			//TODO: Load TODO-List & Kommentarliste;
+			// TODO: Load TODO-List & Kommentarliste;
 		}
 	}
-	
-	
+
 }
