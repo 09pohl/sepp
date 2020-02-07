f5ed40ac7a7ff6f1234bd411cc4a6850dd67ba08
Daniel Schmidt <dniosc@gmail.com>
2020-02-07 11:53:43 +0100

    Pfeilbuttons funktionieren fast - User fehlt

--- sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/FullTextAndEdit.java
+++ sepp-gui/src/main/java/de/verbund/sepp/gui/todo/comment/FullTextAndEdit.java
@@ -29,31 +29,35 @@ public class FullTextAndEdit extends JDialog {
 	private JEditorPane editor;
 	private SEPPMainDlg seppMainDlg;
 	private DatenSchnittstelle datenSchnittstelle = DatenSchnittstelleImpl.getInstance();
-	
-	public FullTextAndEdit(int toDifferentTables, JTable table, SEPPMainDlg seppMainDlg) {
+	private static int row;
+
+	public FullTextAndEdit(int toDifferentTables, JTable table, SEPPMainDlg seppMainDlg, int call) {
 		this.table = table;
+		checkCallSituation(call);
+
+		System.out.println(row);
 		this.seppMainDlg = seppMainDlg;
 		try {
-			createJDialog(toDifferentTables, table);
+			createJDialog(toDifferentTables, table, call);
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(this, "Fehler: Editieren nicht möglich", "ERROR", JOptionPane.ERROR_MESSAGE);
 		}
 
 	}
 
-	private void createJDialog(int toDifferentTables, JTable table) throws IOException {
+	private void createJDialog(int toDifferentTables, JTable table, int call) throws IOException {
 		setSize(400, 200);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setModal(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLayout(new BorderLayout());
-		add(northPanel(), BorderLayout.NORTH);
-		add(centralPanel(table), BorderLayout.CENTER);
+		add(northPanel(toDifferentTables), BorderLayout.NORTH);
+		add(centralPanel(table, call), BorderLayout.CENTER);
 		add(southPanel(toDifferentTables), BorderLayout.SOUTH);
 
 		String user = table.getValueAt(table.getSelectedRow(), 0).toString();
-		
+
 		if (toDifferentTables == 0) {
 			setTitle("Kommentar [Autor: " + user + "]");
 		} else {
@@ -64,20 +68,32 @@ public class FullTextAndEdit extends JDialog {
 
 	}
 
-	private Component northPanel() {
+	private Component northPanel(int toDifferentTables) {
 		JPanel northPanel = new JPanel();
-		northPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		northPanel.add(new JLabel("Bearbeiten:")); //statt Betreff:
+		northPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		JButton nextComment = new JButton("->");
+		JButton previousComment = new JButton("<-");
+		nextComment.addActionListener(e -> next(toDifferentTables, 1));
+		previousComment.addActionListener(e -> next(toDifferentTables, -1));
+		northPanel.add(new JLabel("Bearbeiten:")); // statt Betreff:
+		northPanel.add(previousComment);
+		northPanel.add(nextComment);
 //		northPanel.add(new JTextField(10));
 		return northPanel;
 	}
 
-	private Component centralPanel(JTable table) {
+	private void next(int toDifferentTables, int call) {
+		dispose();
+		new FullTextAndEdit(toDifferentTables, table, seppMainDlg, call);
+
+	}
+
+	private Component centralPanel(JTable table, int call) {
 		JPanel centralPanel = new JPanel();
 		editor = new JEditorPane();
 		editor.setSize(300, 200);
 		editor.setPreferredSize(new Dimension(300, 95));
-		editor.setText((table.getValueAt(table.getSelectedRow(), 1).toString()));
+		editor.setText((table.getValueAt(row, 1).toString()));
 		JScrollPane scrollPane = new JScrollPane(editor);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		centralPanel.add(scrollPane);
@@ -110,5 +126,26 @@ public class FullTextAndEdit extends JDialog {
 			e.printStackTrace();
 		}
 		dispose();
+	}
+
+	private void checkCallSituation(int call) {
+		if (call == 0) {
+			row = table.getSelectedRow();
+			System.out.println("0");
+		} else {
+			if (row == table.getRowCount() - 1 && call == 1) {
+				row = 0;
+			} else if (row == 0 && call == -1) {
+				row = table.getRowCount() - 1;
+			} else if (row != 0 && call == 1) {
+				row += 1;
+			} else if (row != 0 && call == -1) {
+				row -= 1;
+			} else if (row == 0 && call == 1) {
+				row += 1;
+			}
+
+		}
+
 	}
 }
